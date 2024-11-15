ee45117eeeca3276fbea0bde576137cca4000df5
Belikhun <belivipro9x99@gmail.com>
2024-11-15 12:45:40 +0700

    🐶 fail-safe watchdog

--- watchdog.py
+++ watchdog.py
@@ -11,30 +11,33 @@ MONITOR_WS = False
 LAST_WS_HEARTBEAT = 0
 
 async def watchdog_loop():
+	global MONITOR_WS, LAST_WS_HEARTBEAT
 	gc.enable()
 
 	while True:
-		global MONITOR_WS, LAST_WS_HEARTBEAT
-		cleaned = gc.collect()
+		try:
+			cleaned = gc.collect()
 
-		if cleaned:
-			log("INFO", f"Collected and cleaned {cleaned} dangling objects")
+			if cleaned:
+				log("INFO", f"Collected and cleaned {cleaned} dangling objects")
 
-		timestamp = int(time.time_ns() / 1000000)
-		temp = (esp32.raw_temperature() - 32.0) / 1.8
-		log("DEBG", "TIME: {}".format(timestamp))
-		log("DEBG", " CPU: freq {}, temp {:5.1f}C".format(machine.freq(), temp))
+			timestamp = int(time.time_ns() / 1000000)
+			temp = (esp32.raw_temperature() - 32.0) / 1.8
+			log("DEBG", "TIME: {}".format(timestamp))
+			log("DEBG", " CPU: freq {}, temp {:5.1f}C".format(machine.freq(), temp))
 
-		if (MONITOR_WS):
-			heartbeat = timestamp - LAST_WS_HEARTBEAT
-			log("DEBG", "  WS: d_heartbeat={}".format(heartbeat))
+			if (MONITOR_WS):
+				heartbeat = timestamp - LAST_WS_HEARTBEAT
+				log("DEBG", "  WS: d_heartbeat={}".format(heartbeat))
 
-			if (heartbeat > 10000):
-				log("WARN", "Websocket lost heartbeat for more than 10 seconds, will attempt to reconnect now")
-				await client.ws_stop_loop()
-				client.ws_do_reconnect()
+				if (heartbeat > 10000):
+					log("WARN", "Websocket lost heartbeat for more than 10 seconds, will attempt to reconnect now")
+					await client.ws_stop_loop()
+					client.ws_do_reconnect()
+		except Exception as e:
+			log("WARN", f"Watchdog crashed with error: {e}")
 
-		await asyncio.sleep(10)
+		await asyncio.sleep(5)
 
 def start_watchdog():
 	asyncio.create_task(watchdog_loop())
@@ -55,5 +58,5 @@ def ws_heartbeat():
 
 	if not MONITOR_WS:
 		return
-	
+
 	LAST_WS_HEARTBEAT = int(time.time_ns() / 1000000)
